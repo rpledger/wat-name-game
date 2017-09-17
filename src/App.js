ae0d7499d274e6974e19e0d62c757a1687fe787b
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-09-17 11:37:02 -0500

    Install axios; output api data in with console log

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 import React, { Component } from 'react';
 import Game from './Components/Game'
 import './App.css';
+var axios = require('axios');
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
 			people: []
-		}
+		};
 	}
 
 	// Do AJAX call here (lifecycle method) componentWillMount()
-	componentWillMount() {
-		this.setState({people: [
-			{
-				firstName: 'Jane',
-				lastName: 'Doe'
-			},
-			{
-				firstName: 'Joe Person',
-				lastName: 'Person'
-			},
-			{
-				firstName: 'Sally',
-				lastName: 'Seashells'
-			},
-			{
-				firstName: 'John',
-				lastName: 'Smith'
-			},
-			{
-				firstName: 'Suzy',
-				lastName: 'Smith'
-			},
-			{
-				firstName: 'Sam',
-				lastName: 'Person'
-			}
-		]})
+	componentDidMount() {
+		axios.get('https://willowtreeapps.com/api/v1.0/profiles/').then( response => console.log(response));
+		// this.setState({people: [
+		// 	{
+		// 		firstName: 'Jane',
+		// 		lastName: 'Doe'
+		// 	},
+		// 	{
+		// 		firstName: 'Joe Person',
+		// 		lastName: 'Person'
+		// 	},
+		// 	{
+		// 		firstName: 'Sally',
+		// 		lastName: 'Seashells'
+		// 	},
+		// 	{
+		// 		firstName: 'John',
+		// 		lastName: 'Smith'
+		// 	},
+		// 	{
+		// 		firstName: 'Suzy',
+		// 		lastName: 'Smith'
+		// 	},
+		// 	{
+		// 		firstName: 'Sam',
+		// 		lastName: 'Person'
+		// 	}
+		// ]})
 	}
 
 	practice() {
@@ -51,7 +53,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>The Name Game!</h1>
+				<h1>The Name Game</h1>
 				<button onClick={this.practice.bind(this)} type="button" className="btn btn-primary">Practice</button>
 				<button onClick={this.timedStart.bind(this)} type="button" className="btn btn-primary">Start</button>
 				<Game people={this.state.people}/>
